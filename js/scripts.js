dc7362f2463685c045f5434c58e4067776d05238
creightonja <creightonja@gmail.com>
2015-09-11 09:07:25 -0700

    Finish jquery and project

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,21 @@ function replacer(sentence, unword, reword){
   var newSentence = sentence.replace(unword, reword);
   return newSentence;
 };
+
+$(document).ready(function() {
+
+  $("form#replacer").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var unword = $("input#unword").val();
+    var reword = $("input#reword").val();
+    var newSentence = replacer(sentence, unword, reword);
+    $(".input").empty();
+    $(".input").text(sentence);
+
+    $(".result").empty();
+    $(".result").text(newSentence);
+
+    $(".showme").show();
+    event.preventDefault();
+  });
+});
