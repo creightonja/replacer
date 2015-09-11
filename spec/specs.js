eb7fc748c5956d5b2b36f59f410c90127777a2fa
creightonja <creightonja@gmail.com>
2015-09-11 08:53:40 -0700

    adds pointless third test to make more commits

--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('replacer', function() {
   it("Replaces hello world with hello universe", function() {
     expect(replacer("hello world", "world", "universe")).to.equal("hello universe");
   });
+
+  it("Replaces This assignment is boring with This asssignment is really boring", function() {
+    expect(replacer("This assignment is boring", "boring", "really boring")).to.equal("This assignment is really boring");
+  });
 });
