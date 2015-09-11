c524e42c124c1a128b90005b2008fb82573badec
creightonja <creightonja@gmail.com>
2015-09-11 08:49:35 -0700

    finish all two tests and inserts readme

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function replacer(sentence, unword, reword){
+  var newSentence = sentence.replace(unword, reword);
+  return newSentence;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('replacer', function() {
-    it("Replaces hello with goodbye", function() {
-        expect(replacer("hello")).to.equal("goodbye");
-    });
+  it("Replaces hello with goodbye", function() {
+    expect(replacer("hello", "hello", "goodbye")).to.equal("goodbye");
+  });
+
+  it("Replaces hello world with hello universe", function() {
+    expect(replacer("hello world", "world", "universe")).to.equal("hello universe");
+  });
 });
